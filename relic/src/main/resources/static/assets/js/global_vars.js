56ac8f010db5c770890ed5f8ab2a38267b117025
Ramiz A <ramiz.demiurge@gmail.com>
2021-11-20 22:58:30 +0300

    feature: markdown (#34)
    * Add markdown support
    
    * Add markdown support
    
    * Fix markdown bug

--- relic/src/main/resources/static/assets/js/global_vars.js
+++ relic/src/main/resources/static/assets/js/global_vars.js
@@ -1,2 +1,11 @@
-const PERSON_CONTENT_MENU = Object.freeze({"FIELDS":1, "INFO":2, "CALENDAR":3});
+const PERSON_CONTENT_MENU = Object.freeze({
+    "FIELDS": 1,
+    "INFO": 2,
+    "CALENDAR": 3
+});
+
+const PERSON_INFO_VIEW_TYPE = Object.freeze({
+    "READING": 1,
+    "EDITING": 2,
+});
 
